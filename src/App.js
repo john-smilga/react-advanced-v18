1b1d8397cdba6bc25f3550e7ad6cf44f20018c99
john smilga <johnsmilga@gmail.com>
2022-08-04 14:51:58 -0700

    removed small bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Final from './tutorial/11-react-router/final/index'
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-      <Final />
     </div>
   )
 }
